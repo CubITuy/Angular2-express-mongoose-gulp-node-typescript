1fadd8d0b4559706d2f7d039f93248fda98c7284
Martín Sabatella <tinchosoldier@gmail.com>
2016-11-20 15:25:48 -0300

    En server.ts function(err: any

--- server/src/server.js
+++ server/src/server.js
@@ -1,35 +1,27 @@
 /// <reference path="../typings/index.d.ts" />
-
-import express = require('express');
-import BaseRoutes = require("./config/routes/Routes");
-import bodyParser = require("body-parser");
-
-import path = require('path');
-var port: number = process.env.PORT || 3000;
-var env:string = process.env.NODE_ENV || 'developement';
-
+"use strict";
+var express = require("express");
+var BaseRoutes = require("./config/routes/Routes");
+var bodyParser = require("body-parser");
+var path = require("path");
+var port = process.env.PORT || 3000;
+var env = process.env.NODE_ENV || 'developement';
 var app = express();
-
+exports.app = app;
 app.set('port', port);
-
 app.use('/app', express.static(path.resolve(__dirname, '../client/app')));
 app.use('/libs', express.static(path.resolve(__dirname, '../client/libs')));
-
 // for system.js to work. Can be removed if bundling.
 app.use(express.static(path.resolve(__dirname, '../client')));
 app.use(express.static(path.resolve(__dirname, '../../node_modules')));
-
 app.use(bodyParser.json());
 app.use('/api', new BaseRoutes().routes);
-
-var renderIndex = (req: express.Request, res: express.Response) => {
+var renderIndex = function (req, res) {
     res.sendFile(path.resolve(__dirname, '../client/index.html'));
-}
-
+};
 app.get('/*', renderIndex);
-
-if(env === 'developement'){
-    app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+if (env === 'developement') {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.json({
             error: err,
@@ -37,22 +29,18 @@ if(env === 'developement'){
         });
     });
 }
-
-
 // catch 404 and forward to error handler
-app.use(function(req: express.Request, res: express.Response, next) {
-    let err = new Error("Not Found");
+app.use(function (req, res, next) {
+    var err = new Error("Not Found");
     next(err);
 });
-
 // production error handler
 // no stacktrace leaked to user
-app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
         error: {},
         message: err.message
     });
 });
-
-export { app }
+//# sourceMappingURL=server.js.map
